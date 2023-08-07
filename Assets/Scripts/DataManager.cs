56edb2fe938fcc588c9eecceb102ccb45bf1f742
LizetMilagros <lcuevas@unsa.edu.pe>
2023-08-07 08:19:43 -0500

    modificaciones

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    /**[SerializeField] private List<Item> items = new List<Item>();
+    [SerializeField] private GameObject buttonContainer;
+    [SerializeField] private ItemButtonManager itemButtonManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject.instance.OnItemsMenu += CreateButton;
+    }
+    private void CreateButtons(){
+        foreach (var item in items){
+            ItemButtonManager itemButton;
+            itemButton = Instantiate(itemButtonManager, buttonContainer.transform);
+            itemButton.ItemName = item.ItemName;
+            itemButton.ItemDescription = item.ItemDescription;
+            itemButton.ItemImage = item.ItemImage;
+            itemButton.Item3DPosition = item.Item3DPosition
+            itemButton.name = item.ItemName;
+        }
+    }**/
+
+   
+}
